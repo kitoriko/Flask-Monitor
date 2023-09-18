9347ed38499b2c296b7a70b3f4a1f80b30ebd769
Egor Lykov <egor.lykov@glowbyteconsultion.com>
2023-09-18 03:38:08 +0300

    test telegram

--- app.py
+++ app.py
@@ -6,6 +6,20 @@ from datetime import datetime, timezone
 app = Flask(__name__)
 logging.basicConfig(filename='app.log', level=logging.DEBUG)
 
+import requests
+
+TELEGRAM_TOKEN = 'YOUR_TELEGRAM_BOT_TOKEN'
+CHAT_ID = 'YOUR_CHAT_ID'
+
+def send_telegram_notification(message):
+    url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
+    data = {
+        "chat_id": CHAT_ID,
+        "text": message
+    }
+    response = requests.post(url, data=data)
+    return response.json()
+
 @app.route('/')
 def index():
     try:
@@ -15,6 +29,12 @@ def index():
         v1 = client.CoreV1Api()
         nodes = v1.list_node().items
         node_info = []
+        for node in node_info:
+            if node['days_passed'] > 7:
+                message = f"Warning! Cluster {node['name']} has been up for more than 7 days (Created on: {node['creation_time']})."
+                send_telegram_notification(message)
+        
+
         for node in nodes:
             creation_time = node.metadata.creation_timestamp
             current_time = datetime.now(timezone.utc)
@@ -22,9 +42,10 @@ def index():
             name = node.metadata.name
             node_info.append({'name': name, 'days_passed': days_passed, 'creation_time': creation_time})
         return render_template('index.html', node_info=node_info)
+
     except Exception as err:
         logging.error(f"ERROR: {err}")
         return f"ERROR: {err}", 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
